2d8c4f59350411ca96f470761567dcf09dc95c7d
Neolivan <neolivan.eng@outlook.com>
2023-11-28 21:05:19 -0300

    session id added

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 /* ***********************
  * Require Statements
  *************************/
+const session = require("express-session")
+const pool = require('./database/')
 const baseController = require("./controllers/baseController");
 const express = require("express");
 const env = require("dotenv").config();
@@ -21,6 +23,30 @@ const customErrorController = require("./controllers/customErrorController");
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout"); // not at views root
+
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
 
 /* ***********************
  * Routes
